37b2ba2f8f9e80a100e3037cabf261b0037453a4
Paul Stadig <paul@stadig.name>
2014-03-26 21:12:18 -0400

    Bump to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjstadig/humane-test-output "0.1.0-SNAPSHOT"
+(defproject pjstadig/humane-test-output "0.1.0"
   :description "Humane test output for clojure.test"
   :url "http://github.com/pjstadig/humane-test-output/"
   :license {:name "Eclipse Public License"
