51e8752e2584c1a35e5f2b215e236ad6298dfc00
Paul Stadig <paul@stadig.name>
2014-03-28 09:01:27 -0400

    Use defmethod instead of creating an add-method function.
    Disable humane output if the INHUMANE_TEST_OUTPUT environment variable
    is defined.

--- src/pjstadig/humane_test_output.clj
+++ src/pjstadig/humane_test_output.clj
@@ -1,52 +1,53 @@
 (ns pjstadig.humane-test-output
-  (:use [clojure.data]
-        [clojure.pprint :only [get-pretty-writer pprint]]
-        [clojure.test]))
-
-(defn add-method [multimethod dispatch-value fn]
-  (.addMethod multimethod dispatch-value fn))
+  (:use [clojure.test])
+  (:require [clojure.data :as data]
+            [clojure.pprint :as pp]))
 
 (defonce activation-body
   (delay
-   (add-method assert-expr '=
-               (fn [msg [_ a & more]]
-                 `(let [a# ~a]
-                    (if-let [more# (seq (list ~@more))]
-                      (let [result# (apply = a# more#)]
-                        (if result#
-                          (do-report {:type :pass, :message ~msg,
-                                      :expected a#, :actual more#})
-                          (do-report {:type :fail, :message ~msg,
-                                      :expected a#, :actual more#,
-                                      :diffs (map vector more# (map #(take 2 (diff a# %)) more#))}))
-                        result#)
-                      (throw (Exception. "= expects more than one argument"))))))
-   (add-method report :fail
-               (fn [{:keys [type expected actual diffs message] :as event}]
-                 (with-test-out
-                   (inc-report-counter :fail)
-                   (println "\nFAIL in" (testing-vars-str event))
-                   (when (seq *testing-contexts*) (println (testing-contexts-str)))
-                   (when message (println message))
-                   (binding [*out* (get-pretty-writer *out*)]
-                     (let [print-expected (fn [actual]
-                                            (print "expected: ")
-                                            (pprint expected)
-                                            (print "  actual: ")
-                                            (pprint actual))]
-                       (if (seq diffs)
-                         (doseq [[actual [a b]] diffs
-                                 :when (or a b)]
-                           (print-expected actual)
-                           (print "    diff:")
-                           (if a
-                             (do (print " - ")
-                                 (pprint a)
-                                 (print "          + "))
-                             (print " + "))
-                           (when b
-                             (pprint b)))
-                         (print-expected actual)))))))))
+   (when (not (System/getenv "INHUMANE_TEST_OUTPUT"))
+     (defmethod assert-expr '= [msg [_ a & more]]
+       `(let [a# ~a]
+          (if-let [more# (seq (list ~@more))]
+            (let [result# (apply = a# more#)]
+              (if result#
+                (do-report {:type :pass, :message ~msg,
+                            :expected a#, :actual more#})
+                (do-report {:type :fail, :message ~msg,
+                            :expected a#, :actual more#,
+                            :diffs (map vector
+                                        more#
+                                        (map #(take 2 (data/diff a# %))
+                                             more#))}))
+              result#)
+            (throw (Exception. "= expects more than one argument")))))
+
+     (defmethod report :fail
+       [{:keys [type expected actual diffs message] :as event}]
+       (with-test-out
+         (inc-report-counter :fail)
+         (println "\nFAIL in" (testing-vars-str event))
+         (when (seq *testing-contexts*) (println (testing-contexts-str)))
+         (when message (println message))
+         (binding [*out* (pp/get-pretty-writer *out*)]
+           (let [print-expected (fn [actual]
+                                  (print "expected: ")
+                                  (pp/pprint expected)
+                                  (print "  actual: ")
+                                  (pp/pprint actual))]
+             (if (seq diffs)
+               (doseq [[actual [a b]] diffs
+                       :when (or a b)]
+                 (print-expected actual)
+                 (print "    diff:")
+                 (if a
+                   (do (print " - ")
+                       (pp/pprint a)
+                       (print "          + "))
+                   (print " + "))
+                 (when b
+                   (pp/pprint b)))
+               (print-expected actual)))))))))
 
 (defn activate! []
   @activation-body)
