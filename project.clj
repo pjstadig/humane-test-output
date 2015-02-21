85b3b93a767e9fe1ba0e2347929c9cd06a8dcc50
Paul Stadig <paul@stadig.name>
2015-02-21 07:08:36 -0500

    Move clojure into dev dependencies.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://github.com/pjstadig/humane-test-output/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.3.0"]]}})
