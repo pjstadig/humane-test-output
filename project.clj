2e3e1da2b137361c6d4172a98f41cdde89bef23f
Paul Stadig <paul@stadig.name>
2015-02-24 09:56:57 -0500

    Add clojars as release repository.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/pjstadig/humane-test-output/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :test {:injections [(require 'pjstadig.humane-test-output)
                                  (pjstadig.humane-test-output/activate!)]}})
