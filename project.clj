88d205147e5fb209821196a7ad128e1d574b926d
Paul Stadig <paul@stadig.name>
2016-02-18 13:40:21 -0500

    I'm still too lazy to write proper tests...
    but at least I'm more explicit now.
    
    Fixes #7

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
+  :test-selectors
+  {:default (complement :intentionally-failing)
+   :yes-i-know-the-tests-are-supposed-to-fail :intentionally-failing}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :test {:injections [(require 'pjstadig.humane-test-output)
                                  (pjstadig.humane-test-output/activate!)]}})
--- test/pjstadig/humane_test_output/formatting_test.clj
+++ test/pjstadig/humane_test_output/formatting_test.clj
@@ -1,8 +1,11 @@
 (ns pjstadig.humane-test-output.formatting-test
   (:use [clojure.test]))
 
-(deftest t-formatting
-  (testing "FIXME, I fail."
+(deftest t-nothing-to-see-here
+  (is true "everything should be A-OK"))
+
+(deftest ^:intentionally-failing t-formatting
+  (testing "THESE TESTS ARE INTENDED TO FAIL"
     (is (= {:foo :bar :baz :quux :something "a long string?"
             :another-key "and another value"}
            {:fo :bar}))
@@ -21,6 +24,6 @@
 (defmacro deftest+
   [test-name expected actual]
   `(deftest ~test-name
-     (is (= ~expected ~actual))))
+     (is (= ~expected ~actual) "THIS ONE SHOULD ALSO FAIL")))
 
-(deftest+ t-macro-wrapping 1 2)
+(deftest+ ^:intentionally-failing t-macro-wrapping 1 2)
